b724b2e9a1764b0bba34e84f9a75868f0907689c
Gabriel Barboza <gabrielmbarboza@gmail.com>
2019-06-04 23:32:45 -0300

    Cria campo role em User

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
+  enum role: %i[user admin]
+
+  after_initialize do
+    self.role ||= :user if new_record?
+  end
 end
