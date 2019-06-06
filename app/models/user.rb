1f5d44dfad44ac8b22e2f349d5ee26eb155f640f
Gabriel Barboza <gabrielmbarboza@gmail.com>
2019-06-06 00:55:38 -0300

    Implementando soft delete

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,23 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
+  
+  
   enum role: %i[user admin]
 
   after_initialize do
     self.role ||= :user if new_record?
+  end
+
+  def soft_delete
+    update_attribute(:deleted_at, Time.current)
+  end
+
+  def active_for_authentication?
+    super && !deleted_at
+  end
+
+  def inactive_message
+    !deleted_at ? super : :deleted_account
   end
 end
