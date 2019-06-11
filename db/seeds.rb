db4dab302e46a93901f6837ea3d56775cd31a3d0
Gabriel Barboza <gabriel@adena.com.br>
2019-06-11 16:04:57 -0300

    Adiciona campos first_name e last_name em User no seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,22 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
-User.create(email: "admin@ruptiva.com.br", password: "ruptiva",  password_confirmation: "ruptiva", role: :admin)
+User.create(
+  email: "admin@ruptiva.com.br",
+  first_name: "Admin",
+  last_name: "Ruptiva",
+  password: "ruptiva",
+  password_confirmation: "ruptiva",
+  role: :admin
+)
 
 15.times do
     password = Faker::Internet.password
     
     User.create(
-      email: Faker::Internet.email, 
+      email: Faker::Internet.email,
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name, 
       password: password,
       password_confirmation: password, 
       role: :user
