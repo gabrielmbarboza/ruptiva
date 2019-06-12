ba805b313f75f82d1c8cb739d172902b4d183e3b
Gabriel Barboza <gabrielmbarboza@gmail.com>
2019-06-11 22:02:00 -0300

    Ajusta rotas do devise para api/v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth', controllers: { registrations: 'users/registrations' }
   namespace :api do
     namespace :v1 do 
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: { registrations: 'api/v1/users/registrations' }
       resources :users
     end
   end
